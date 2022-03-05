1d3f2a7ee8b69c1660fc89fdbb040cb09cea0254
manybence <b13many@berzsenyi.hu>
2022-03-05 20:55:13 +0100

    H-table, Pearson coeff added

--- side_channel_attack/power_analysis.py
+++ side_channel_attack/power_analysis.py
@@ -2,14 +2,45 @@
 """
 Created on Wed Mar  2 14:30:28 2022
 
-PRESENT S-box power consuption analysis
-@author: Bence Mány
+Side-channel attack
+Power analysis of AES
+@author: Bence Mány, Vicente Bartual Ferran
 """
-import random
 
-def hw(number):
+import csv
+import constants
+import scipy.stats
+
+#Static variables
+path_traces = "data/T3.dat"
+path_inputs = "data/inputs3.dat"
+s_box = constants.S
+inv_s_box = constants.SI
+key = 0x5b
+
+
+#Global variables
+traces = []
+inputs = []
+H_table =[]
+
+
+
+#Reading the table of power traces
+def read_traces(): 
+    with open(path_traces, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        for row in spamreader:
+            line = []
+            for elements in row:
+                line.append(float(elements))
+            traces.append(line)
+
+    
+#Calculating the Hamming-weight of a given byte
+def hamming_weight(byte):
     weight = 0
-    for i in (bin(number)):
+    for i in (bin(byte)):
         try:
             if int(i) == 1:
                 weight += 1
@@ -17,17 +48,49 @@ def hw(number):
             continue
     return weight
 
-key = 0xb
-S_box = [0xc, 0x5, 0x6, 0xb, 0x9, 0x0, 0xa, 0xd, 0x3, 0xe, 0xf, 0x8, 0x4, 0x7, 0x1, 0x2]
-traces = []
 
-random.seed()
-
-for i in range(50):
-    plain = int(random.random()*15)
-    x = key ^ plain
-    out = S_box[x]
-    traces.append(hw(out))
+#Reading the plaintext inputs
+def read_inputs(): 
+    with open(path_inputs, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        for row in spamreader:
+            for elements in row:
+                inputs.append(int(elements))
+                
+#Calculating the expected Hamming-weights for each input with each possible key          
+def calculate_h_table():
+    for a in inputs:
+        line = []
+        for keys in range(256):
+            line.append(hamming_weight(s_box[a ^ keys]))
+        H_table.append(line)
+        
+        
+#Calculating the Pearson correlation coefficient between the predicted and measured power traces for each possible key
+def calculate_coeff():
+    predicted = []
+    sampled = []
+    coeff = []
+    for lines in H_table:
+        predicted.append(lines[0])
+    for lines in traces:
+        sampled.append(lines[0])
+    coeff = scipy.stats.pearsonr(predicted, sampled)[0]
+    print(coeff)
+            
     
-print(traces)
+
+
+
+#Main function*****************************************************************************
+
+read_traces()
+read_inputs()
+calculate_h_table()
+calculate_coeff()
+
+
+
+    
+
 
