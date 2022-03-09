101628906a3649ef7da55053d958201a18cf8ad9
manybence <b13many@berzsenyi.hu>
2022-03-09 14:12:56 +0100

    Return values

--- side_channel_attack/power_analysis.py
+++ side_channel_attack/power_analysis.py
@@ -19,16 +19,10 @@ inv_s_box = constants.SI
 key = 0x5b
 
 
-#Global variables
-traces = []
-inputs = []
-H_table =[]
-coeffs = []
-
-
 
 #Reading the table of power traces
-def read_traces(): 
+def read_traces(path_traces): 
+    traces = []
     with open(path_traces, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
         for row in spamreader:
@@ -36,6 +30,7 @@ def read_traces():
             for elements in row:
                 line.append(float(elements))
             traces.append(line)
+    return traces
 
     
 #Calculating the Hamming-weight of a given byte
@@ -51,25 +46,29 @@ def hamming_weight(byte):
 
 
 #Reading the plaintext inputs
-def read_inputs(): 
+def read_inputs(path_inputs): 
+    inputs = []
     with open(path_inputs, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
         for row in spamreader:
             for elements in row:
                 inputs.append(int(elements))
+    return inputs
                 
 #Calculating the expected Hamming-weights for each input with each possible key          
-def calculate_h_table():
+def calculate_h_table(inputs):
+    H_table = []
     for a in inputs:
         line = []
         for keys in range(256):
             line.append(hamming_weight(s_box[a ^ keys]))
         H_table.append(line)
+    return H_table
         
         
 #Calculating the Pearson correlation coefficient between the predicted and measured power traces for each possible key
 def calculate_coeff():
-    
+    coeffs = []
     #Extract predictions for all keys
     for key in range(len(H_table[0])):
         
@@ -86,6 +85,7 @@ def calculate_coeff():
                 sampled.append(lines[i])
             coeff_i.append(scipy.stats.pearsonr(predicted, sampled)[0])
         coeffs.append(max(coeff_i))
+    return coeffs
     
             
     
@@ -94,12 +94,13 @@ def calculate_coeff():
 
 #Main function*****************************************************************************
 
-read_traces()
-read_inputs()
-calculate_h_table()
-calculate_coeff()
-print(coeffs)
-print(max(coeffs))
+traces = read_traces(path_traces)
+inputs = read_inputs(path_inputs)
+H_table = calculate_h_table(inputs)
+coeffs = calculate_coeff()
+print(H_table[0])
+#print(coeffs)
+#print(max(coeffs))
 
 
 
