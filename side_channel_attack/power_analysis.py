ed2065c9c71a77716c39d2e5a22cf22d669cf63a
Vicente Bartual <vicentebartualf@gmail.com>
2022-03-09 17:29:46 +0100

    Add function documentation

--- side_channel_attack/power_analysis.py
+++ side_channel_attack/power_analysis.py
@@ -7,10 +7,10 @@ Power analysis of AES
 @author: Bence Mány, Vicente Bartual Ferran
 """
 
-import csv
 import math
 import constants
 import scipy.stats
+from typing import List
 
 #Static variables
 path_traces = "data/T3.dat"
@@ -21,72 +21,113 @@ key = 0x5b
 
 
 
-#Reading the table of power traces
-def read_traces(path_traces): 
+def read_traces(path_traces: str) -> List[float]:
+    """ Reads and parses the power traces data file.
+
+    :param path_traces: Path to power traces file.
+    :type path_traces: str
+
+    :return: A 2-dimension list. The outer list contains 55 lists, each of them
+             containing 600 values. This way, it will be easier to get the 600
+             traces related to the inputs for each trace time.
+    :rtype: List[List[float]]
+    """
+    with open(path_traces) as f:
+        lines = f.readlines()
+
     traces = []
-    with open(path_traces, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        for row in spamreader:
-            line = []
-            for elements in row:
-                line.append(float(elements))
-            traces.append(line)
+    for line in lines:
+        input_traces = [float(t) for t in line.split(',')]
+        for i in range(len(input_traces)):
+            if i >= len(traces):
+                traces.append([])
+            traces[i].append(input_traces[i])
     return traces
 
+
+def read_inputs(path_inputs: str) -> List[float]:
+    """ Reads the inputs data file.
+
+    :param path_inputs: Path to inputs file.
+    :type path_inputs: str
+
+    :return: A list containing the 600 input values.
+    :rtype: List[float]
+    """
+    with open(path_inputs) as f:
+        data = f.read()
     
-#Calculating the Hamming-weight of a given byte
-def hamming_weight(byte):
+    inputs = []
+    for n in data.split(','):
+        inputs.append(int(n))
+    return inputs
+
+    
+def hamming_weight(byte: int) -> int:
+    """ Calculates the Hamming Weight (number of 1 bits) of the given byte.
+
+    :param byte: Byte from which to calculate the Hamming Weight.
+    :type byte: int
+
+    :return: The number of 1s in byte.
+    :rtype: int
+    """
     weight = 0
-    for i in (bin(byte)):
-        try:
-            if int(i) == 1:
-                weight += 1
-        except:
-            continue
+    for i in range(8):
+        weight += (byte >> i) & 0x01
     return weight
 
 
-#Reading the plaintext inputs
-def read_inputs(path_inputs): 
-    inputs = []
-    with open(path_inputs, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        for row in spamreader:
-            for elements in row:
-                inputs.append(int(elements))
-    return inputs
-                
-#Calculating the expected Hamming-weights for each input with each possible key          
-def calculate_h_table(inputs):
-    H_table = []
-    for a in inputs:
-        line = []
-        for keys in range(256):
-            line.append(hamming_weight(s_box[a ^ keys]))
-        H_table.append(line)
-    return H_table
+def calculate_h_table(inputs: List[int]) -> List[List[int]]:
+    """ Calculates the H table for the given input list. Each input value is
+    xored with every possible key and then is substituted using the AES S-box.
+    The final values of the H table are the Hamming Weights of those results.
+
+    :param inputs: The input list.
+    :type inputs: List[int]
+
+    :return: A 2-dimension list. The outer list contains 256 lists (each one for
+    each key, in order), and the inner lists the resulting Hamming Weights
+    for its key and each input.
+    :rtype: List[List[int]]
+    """
+    h_table = []
+    for key in range(256):
+        if key >= len(h_table):
+            h_table.append([])
+        for inp in inputs:
+            h_table[key].append(hamming_weight(s_box[inp ^ key]))
+    return h_table
         
         
-#Calculating the Pearson correlation coefficient between the predicted and measured power traces for each possible key   
-def corr(h_values, t_values):
-    assert(len(h_values) == len(t_values))
-    assert(len(h_values) == 600)
+def pearson_correlation(data_1: List[float], data_2: List[float]) -> float:
+    """ Calculates the pearson correlation coefficient between the two input
+    data sets. The data sets must have the same number of elements.
 
-    h_avg = sum(h_values) / len(h_values)
-    t_avg = sum(t_values) / len(t_values)
+    :param data_1: First data set.
+    :type data_1: List[float|int]
+    :param data_2: Second data set.
+    :type data_2: List[float|int]
 
-    h_var = 0.0
-    t_var = 0.0
-    for i in range(len(h_values)):
-        h_var += (h_values[i] - h_avg)*(h_values[i] - h_avg)
-        t_var += (t_values[i] - t_avg)*(t_values[i] - t_avg)
-    den = math.sqrt(h_var*t_var)
+    :return: Pearson correlation coefficient.
+    :rtype: float
+    """
+    assert(len(data_1) == len(data_2))
 
-    corr = 0.0
-    for i in range(len(h_values)):
-        corr += (h_values[i]-h_avg)*(t_values[i]-t_avg)
-    corr /= den
-    return corr
+    mean_1 = sum(data_1) / len(data_1)
+    mean_2 = sum(data_2) / len(data_2)
+
+    variance_1 = 0.0
+    variance_2 = 0.0
+    for i in range(len(data_1)):
+        variance_1 += (data_1[i] - mean_1)*(data_1[i] - mean_1)
+        variance_2 += (data_2[i] - mean_2)*(data_2[i] - mean_2)
+
+    covariance = 0.0
+    for i in range(len(data_1)):
+        covariance += (data_1[i] - mean_1)*(data_2[i] - mean_2)
+    
+    return covariance / math.sqrt(variance_1*variance_2)
 
 
 #Finding the highest correlation among possible key values. 
@@ -103,27 +144,20 @@ def key_predict(coeffs):
     return key_pred, best
 
 
-#Main function*****************************************************************************
-
 def main():
+    """ Main function. """
     traces = read_traces(path_traces)
     inputs = read_inputs(path_inputs)
-    H_table = calculate_h_table(inputs)
+    h_table = calculate_h_table(inputs)
     coeffs = []
 
     #Extract predictions for each possible key
-    for key in range(len(H_table[0])):        
-        predicted_i = []
-        for lines in H_table:
-            predicted_i.append(lines[key])    
-        #Extract each time samples for the given key
+    for key_guess in h_table:
         coeff_i = []
-        for i in range(len(traces[0])):
-            sampled = []
-            for lines in traces:
-                sampled.append(lines[i])        
-            #Calculate correlation between the prediction and the current time sample
-            coeff_i.append(abs(corr(predicted_i, sampled)))
+        for samples in traces:
+            #Calculate correlation between the key guess and the current time sample
+            #coeff_i.append(scipy.stats.pearsonr(predicted_i, sampled)[0])
+            coeff_i.append(abs(pearson_correlation(key_guess, samples)))
         coeffs.append(coeff_i)
         
     #Determining the most likely key candidate
@@ -131,15 +165,5 @@ def main():
     print("The most likely key candidate is: ", key, "\nwith the correlation value: ", correlation)
 
 
-
-
 if __name__ == "__main__":
     main()
-            
-
-
-
-
-    
-
-
