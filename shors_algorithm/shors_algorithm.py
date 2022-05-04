46d64e349953e84dcc55ca41a11ae5abe5a5fa25
Vicente Bartual <vicentebartualf@gmail.com>
2022-05-04 14:35:14 +0200

    Average

--- shors_algorithm/shors_algorithm.py
+++ shors_algorithm/shors_algorithm.py
@@ -6,7 +6,7 @@ Implementation of cracking RSA by using Shor's algorithm
 @author: Bence Mány, Vicente Bartual Ferran
 """
 
-from typing import List
+import sys
 import math
 import sympy
 from random import randrange
@@ -14,29 +14,22 @@ import timeit
 import matplotlib.pyplot as plt
 
 
-#TODO: Simulation for different key sizes -> time elapsed - Brute-force vs Shor's
-#TODO: Proper RSA encryption?
-#TODO: Proper key generation
-#TODO: Smarter brute force attack? Storing prime numbers?
-
-#RSA key size = 1028 bit
-#Recommended maximum size = 20 bit
-key_size = 18
-
-def generate_keys(key_size: int) -> int:
+def generate_keys(key_size: int) -> "tuple[int, int, int]":
     """
-    Generating two unique prime numbers (the secret keys), then combining them to create the public key.
+    Generating two unique prime numbers (the secret keys), then combining them
+    to create the public key.
 
     Parameters
     ----------
     size : int
-        The size of the public key (in bits). RSA uses 1028 bits, but that would be uncrackable by a normal computer. Recommended value is max 60.
+        The size of the public key (in bits). RSA uses 1028 bits, but that would
+        be uncrackable by a normal computer. Recommended value is max 60.
 
     Returns
     -------
-    int
-        The generated public key, which is the product of the two private keys.
-
+    tuple[int, int, int]
+        It returns three values: n, the product of the prime numbers; e, the
+        encryption exponent and d, the decryption exponent.
     """
     if key_size >= 4:
         size = 2**int(key_size/2)
@@ -59,32 +52,49 @@ def generate_keys(key_size: int) -> int:
         return n, e, d
     else:
         print("Incorrect key size. It has to be at least 4 bits.")
-        return -1
+        sys.exit(1)
 
 
-def calculate_e_and_d(p: int, q) -> tuple[int, int]:
-    totient_func = math.lcm(p-1, q-1)
+def calculate_e_and_d(p: int, q: int) -> "tuple[int, int]":
+    """
+    Calculates the RSA numbers e and d, part of the public and private key
+    respectively.
+
+    Parameters
+    ----------
+    p : int
+        The first prime number.
+    q: int
+        The second prime number.
+
+    Returns
+    -------
+    tuple[int, int]
+        It returns two values: e, the encryption exponent and d, the decryption
+        exponent.
+    """
+    totient_n = math.lcm(p-1, q-1)
+
+    # e has to be coprime with the totient of n
     e = 3
     while True:
-        if sympy.isprime(e) and math.gcd(totient_func, e) == 1:
+        if sympy.isprime(e) and math.gcd(totient_n, e) == 1:
             break
         e += 1
-    d = multiplicative_inverse(e, totient_func)
-    return e, d
 
-
-def multiplicative_inverse(e, phi):
+    # d is calculated as the multiplicative inverse of e
     d = 0
     x1 = 0
     x2 = 1
     y1 = 1
-    temp_phi = phi
+    temp_e = e
+    temp_totient = totient_n
 
-    while e > 0:
-        temp1 = temp_phi//e
-        temp2 = temp_phi - temp1*e
-        temp_phi = e
-        e = temp2
+    while temp_e > 0:
+        temp1 = temp_totient//temp_e
+        temp2 = temp_totient - temp1*e
+        temp_totient = temp_e
+        temp_e = temp2
 
         x = x2 - temp1 * x1
         y = d - temp1 * y1
@@ -94,18 +104,19 @@ def multiplicative_inverse(e, phi):
         d = y1
         y1 = y
 
-    if temp_phi == 1:
-        return d + phi
+    if temp_totient == 1:
+        d += totient_n
+    return e, d
 
-  
-def brute_force(n: int) -> tuple[int, int]:
+
+def brute_force(n: int) -> "tuple[int, int]":
     """
     Finds the two prime factors of the given number n by brute force.
 
     Parameters
     ----------
     n : int
-        The number which needs to be factorized. It is the product of two unique primes.
+        The number to be factorized. It is the product of two unique primes.
 
     Returns
     -------
@@ -114,14 +125,13 @@ def brute_force(n: int) -> tuple[int, int]:
     """
     for i in range(2, n):
         if n % i == 0:
-            return i, int(n/1)
-    print("ERROR!")
+            return i, n//1
   
 
 def find_period(a: int, n: int) -> int:
     """
-    Finds the smallest 't' value that satisfies: (a**t)%N = 1
-    This is the quantum step. It can take loooong time.
+    Finds the smallest 't' value that satisfies: (a**t)%N = 1.
+    This is the quantum step. In a regular computer, it takes a long time.
 
     Parameters
     ----------
@@ -140,8 +150,13 @@ def find_period(a: int, n: int) -> int:
     print("Searching for period...")
     while True:
         t += 1
-        if (a**t)%n == 1:
+        if (a**t) % n == 1:
             return t
+        if t % 50000 == 0:
+            # If t grows higher than 50000, try with another base a.
+            # In our experiments, this has proven faster than just keep trying
+            # with the same base, increasing t.
+            return 1
 
 
 def shors_algorithm(n: int) -> int:
@@ -160,25 +175,26 @@ def shors_algorithm(n: int) -> int:
     """
     a = 1
     while True:
-        #a = randrange(n-2) + 2
         a += 1
         print(f"Trying new 'a' value ({a})")
         k = math.gcd(a, n)
-        if k > 1: 
+        if k != 1: 
             factor = k
             break
         else:
             t = find_period(a, n)
-            print(t)
-            if t%2 > 0:
+            if t % 2 > 0:
                 continue
-            elif (a**(t/2))%n == n-1:
+            elif (a**(t//2)) % n == n-1:
                 continue
             else:
-                factor = math.gcd(int(a**(t/2) + 1), n)
+                factor = math.gcd(a**(t//2)+1, n)
                 if factor > 1:
                     break
     return factor, int(n/factor)
+
+
+AVERAGE = 5 # Times run for each key size
 
 def main():
     """ Main function. """
@@ -186,11 +202,12 @@ def main():
     times_shor = []
     times_bf = []
     sizes = []
-    #Recommended range: (4, 19)
-    for i in range(4, 17):
-        if i%2 == 0:
-            sizes.append(i)
-            
+
+    for i in range(4, 21, 2):
+        time_shor_avg = 0
+        time_bf_avg = 0
+        sizes.append(i)
+        for _ in range(AVERAGE):
             print("\n\nGenerating the private and public keys")
             print(f"Key size: {i}")
             n, e, d = generate_keys(i)
@@ -202,7 +219,7 @@ def main():
             print("The private key (n, d) is ({n}, {d})")
             end = timeit.default_timer()
             print("Processing time:", round(end - start, 6), "seconds")
-            times_bf.append(round(end - start, 6))
+            time_bf_avg += end - start
             
             print("\nExecuting Shor's algorithm")
             start = timeit.default_timer()
@@ -211,7 +228,9 @@ def main():
             print("The private key (n, d) is ({n}, {d})")
             end = timeit.default_timer() 
             print("Processing time:", round(end - start, 6), "seconds")
-            times_shor.append(round(end - start, 6))
+            time_shor_avg += end - start
+        times_shor.append(time_shor_avg/AVERAGE)
+        times_bf.append(time_bf_avg/AVERAGE)
         
     plt.plot(sizes, times_shor, label = "Shor's alg.")
     plt.plot(sizes, times_bf, label = "Brute-force")
